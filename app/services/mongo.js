7c85897b6d03b4f2821ac1259b5243ec46af51d6
Evgeny Finkin <jon.finkin@gmail.com>
2022-07-25 18:39:07 +0300

    commite

--- app/services/mongo.js
+++ app/services/mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const url =
-  'mongodb+srv://username:strongpassword@cluster0.0lqfcvh.mongodb.net/?retryWrites=true&w=majority';
+  'mongodb+srv://username:password@cluster0.0lqfcvh.mongodb.net/?retryWrites=true&w=majority';
 
 (async () => {
   try {
